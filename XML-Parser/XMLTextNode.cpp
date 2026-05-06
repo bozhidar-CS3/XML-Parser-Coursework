cc6375355f62b550c753473578bd6ab4c6e75e68
bozhidar-CS3 <jamticks27@gmail.com>
2026-05-06 18:13:45 +0300

    Changed the setter and getter of XMLTextNode.h and cpp and added a check for a valid string.

--- XML-Parser/XMLTextNode.cpp
+++ XML-Parser/XMLTextNode.cpp
@@ -13,15 +13,30 @@ XMLTextNode::XMLTextNode(const std::string& possible_content)
 	content = possible_content;
 }
 
-std::string XMLTextNode::get_XMLTextNode() const
+std::string XMLTextNode::get_content() const
 {
 	return content;
 }
 
-XMLTextNode& XMLTextNode::set_XMLTextNode(const std::string& possible_content)
+XMLTextNode& XMLTextNode::set_content(const std::string& possible_content)
 {
 	// TODO: //Евнтуално проверка за стринга дали е валиден текст ? Може би.
-
-	content = possible_content;
+	if (is_valid_content(possible_content))
+	{
+		content = possible_content;
+	}
+	else
+	{
+		std::cout << "Invalid content! \n";
+	}
 	return *this;
+}
+
+bool XMLTextNode::is_valid_content(const std::string& possible_content) const
+{
+	if (possible_content.find("<") != std::string::npos || possible_content.find(">") != std::string::npos)
+	{
+		return false;
+	}
+	return true;
 }
--- XML-Parser/XMLTextNode.h
+++ XML-Parser/XMLTextNode.h
@@ -8,7 +8,8 @@ class XMLTextNode
 public:
 	XMLTextNode();
 	XMLTextNode(const std::string&);
-	std::string get_XMLTextNode() const;
-	XMLTextNode& set_XMLTextNode(const std::string&);
+	std::string get_content() const;
+	XMLTextNode& set_content(const std::string&);
+	bool is_valid_content(const std::string&) const;
 	//Евнтуално за подобрение може да се insert  или add_after/add_at_the_end
 };
